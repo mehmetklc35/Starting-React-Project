d93b0b2bff1a436186687f708dbffa103c222fc3
mehmet <mehmetkilic35@outlook.com>
2024-06-06 08:26:40 +0300

    React Başlangıç Projemiz Tamamlandı....

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+
+import App from "./App.jsx";
+import "./index.css";
+
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDom.createRoot(entryPoint).render(React.createeElement(App));
